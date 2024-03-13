0538419e246944de79ec8cd8781ae174e2c0628a
jaisonpjacob <jaison.jacob@verteil.com>
2024-03-13 16:35:57 +0530

    Hash Table With Linear Probing

---
+++ src/main/java/org/jpj/datastructure/hashtable/linearprobing/SimpleHashtable.java
@@ -0,0 +1,128 @@
+package org.jpj.datastructure.hashtable.linearprobing;
+
+public class SimpleHashtable {
+
+    private StoredEmployee[] hashtable;
+
+    public SimpleHashtable() {
+        hashtable = new StoredEmployee[10];
+    }
+
+    public void put(String key, Employee employee) {
+        int hashedKey = hashKey(key);
+        if (occupied(hashedKey)) {
+            int stopIndex = hashedKey;
+            if (hashedKey == hashtable.length - 1) {
+                hashedKey = 0;
+            }
+            else {
+                hashedKey++;
+            }
+
+            while (occupied(hashedKey) && hashedKey != stopIndex) {
+                hashedKey = (hashedKey + 1) % hashtable.length;
+            }
+        }
+
+        if (occupied(hashedKey)) {
+            System.out.println("Sorry, there's already an employee at position " + hashedKey);
+        }
+        else {
+            hashtable[hashedKey] = new StoredEmployee(key, employee);
+        }
+    }
+
+    public Employee get(String key) {
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
+        }
+        return hashtable[hashedKey].employee;
+    }
+
+    public Employee remove(String key) {
+        int hashedKey = findKey(key);
+        if (hashedKey == -1) {
+            return null;
+        }
+
+        Employee employee = hashtable[hashedKey].employee;
+        hashtable[hashedKey] = null;
+
+        //Rehashing -Why?
+        /*
+           In linear probing we find the first unoccupied slot when we try to add a data to a position
+           which is already taken. Now when removing, when we exercise linear probing to find the
+           element, if an element from a previously occupied slot is already removed, the probing will
+           fail. For eg, suppose we wanted to add an element to position 1, but that is occupied. Then
+           we probe and see if we can add to postion 2,3,4 but that is also occupied and we store in
+           position 5.
+
+           Now suppose we already deleted data in position 2, and if we try to find the element in
+           position 5 by probing it will fail, because position was used initially since position 2 was
+           occupied. But now since position 2 is empty, the probing will not go to 5 it will stop at 2.
+
+           This is why we use rehashing. So that data is adjusted to its immediate vacant space.
+         */
+        StoredEmployee[] oldHashtable = hashtable;
+        hashtable = new StoredEmployee[oldHashtable.length];
+        for (int i = 0; i < oldHashtable.length; i++) {
+            if (oldHashtable[i] != null) {
+                put(oldHashtable[i].key, oldHashtable[i].employee);
+            }
+        }
+
+        return employee;
+    }
+
+    private int hashKey(String key) {
+        return key.length() % hashtable.length;
+    }
+
+    private int findKey(String key) {
+        int hashedKey = hashKey(key);
+        if (hashtable[hashedKey] != null &&
+                hashtable[hashedKey].key.equals(key)) {
+            return hashedKey;
+        }
+
+        int stopIndex = hashedKey;
+        if (hashedKey == hashtable.length - 1) {
+            hashedKey = 0;
+        }
+        else {
+            hashedKey++;
+        }
+
+        while (hashedKey != stopIndex &&
+                hashtable[hashedKey] != null &&
+                !hashtable[hashedKey].key.equals(key)) {
+            hashedKey = (hashedKey + 1) % hashtable.length;
+        }
+
+        if (hashtable[hashedKey] != null &&
+                hashtable[hashedKey].key.equals(key)) {
+            return hashedKey;
+        }
+        else {
+            return -1;
+        }
+
+    }
+
+    private boolean occupied(int index) {
+        return hashtable[index] != null;
+    }
+
+    public void printHashtable() {
+        for (int i = 0; i < hashtable.length; i++) {
+            if (hashtable[i] == null) {
+                System.out.println("empty");
+            }
+            else {
+                System.out.println("Position " + i + ": " +hashtable[i].employee);
+            }
+        }
+    }
+
+}
