b6f3e91036a4281e3978af138dd15586e1c817a9
jaisonpjacob <jaison.jacob@verteil.com>
2024-06-12 16:25:33 +0530

    BST traversal : Inorder

---
+++ src/main/java/org/jpj/datastructure/trees/binarysearchtrees/traversal/inorder/Tree.java
@@ -0,0 +1,23 @@
+package org.jpj.datastructure.trees.binarysearchtrees.traversal.inorder;
+
+public class Tree {
+
+    private TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        }
+        else {
+            root.insert(value);
+        }
+    }
+
+    public void traverseInOrder() {
+        if (root != null) {
+            root.traverseInOrder();
+        }
+    }
+
+
+}
